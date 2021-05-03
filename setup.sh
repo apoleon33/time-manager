5edaac12b6438c7fed248a52af96a7fc9cc25a78
579 <simondumesny@gmail.com>
2021-05-03 22:45:18 +0200

    erased .desktop now use cron

--- setup.sh
+++ setup.sh
@@ -9,7 +9,6 @@ cd $way
 mv ui.py ~/.screen
 mv screen.sh ~/.screen
 mv link.sh ~/.screen
-mv time-manager.desktop ~/.config/autostart/
 cd
 echo "alias mytime='cd | bash .screen/link.sh' " >> '.zshrc'
 echo "alias mytime='cd | bash .screen/link.sh' " >> '.bashrc'
