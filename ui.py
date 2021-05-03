c3667f197eca8809526cc50559e76e5108ec6e38
579 <simondumesny@gmail.com>
2021-05-03 14:36:37 +0200

    updated the python part

--- ui.py
+++ ui.py
@@ -1,3 +1,7 @@
-with open("time.txt", "r") as file:
-	content=file.readlines()
-print(content)
+character="32"
+file=open("time.txt", "r")
+x=file.readlines()
+x=str(x[-1]).replace('\n', '')
+mi=str(round(int(x)/60,2))
+h=str(round(int(x)/3600,2))
+print("vous avez passé "+x+'s '+mi+' min '+h+' hours')
