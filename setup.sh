f5b40bd028329f51837d66510092de0e57b338a3
579 <simondumesny@gmail.com>
2021-05-03 21:21:02 +0200

    added .desktop and updated setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ then
 fi
 cd $way
 mv ui.py ~/.screen
+mv screen.sh ~/.screen
 mv link.sh ~/.screen
+mv time-manager.desktop ~/.config/autostart/
 cd
 echo "alias mytime='cd | bash .screen/link.sh' " >> '.zshrc'
 echo "alias mytime='cd | bash .screen/link.sh' " >> '.bashrc'
